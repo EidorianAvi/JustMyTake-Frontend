081ad8e7468625976a4c39e64981c828ec54f8b0
Adrian <reneavila1993@gmail.com>
2020-12-07 17:31:00 -0700

    Install react-icons and start building out dropdown navigation on the NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import './NavBar.css';
+import { FaAlignJustify } from 'react-icons/fa'
 
 
 const NavBar = () => {
+
+    const [dropdown, setDropdown] = useState(false);
+
     return (
         <div>
             <nav>
-                <h3>Just My Take</h3>
-                <ul>
-                    <li><NavLink exact to="/">Home</NavLink></li>
-                    <li><NavLink to="/my-takes">My Takes</NavLink></li>
-                    <li><NavLink to="/profile">Profile</NavLink></li>
-                </ul>
+                <section className="Banner-Title">
+                    <h3>Just My Take</h3>
+                </section>
+                <section className="NavBar-Navigation">
+                    <button onClick={() => setDropdown(!dropdown)}>
+                        <FaAlignJustify/>
+                    </button>
+                    { dropdown ? 
+                        <ul className="NavBar-Options">
+                            <li><NavLink exact to="/">Home</NavLink></li>
+                            <li><NavLink to="/my-takes">My Takes</NavLink></li>
+                            <li><NavLink to="/profile">Profile</NavLink></li>
+                        </ul> 
+                        : null
+                    }
+                </section>
             </nav>            
         </div>
     );
