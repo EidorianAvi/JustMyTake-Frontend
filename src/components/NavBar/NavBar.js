0706f2783a83c8a8107eda0c7404fe131cb7b910
Adrian <reneavila1993@gmail.com>
2020-12-07 19:07:55 -0700

    Dropdown menu now functional

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,33 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { NavLink } from 'react-router-dom';
-import './NavBar.css';
 import { FaAlignJustify } from 'react-icons/fa'
+import './NavBar.css';
 
 
 const NavBar = () => {
 
+    const dropdownRef = useRef(null);
     const [dropdown, setDropdown] = useState(false);
 
+    const handleClick = () => setDropdown(!dropdown);
+
     return (
-        <div>
-            <nav>
-                <section className="Banner-Title">
-                    <h3>Just My Take</h3>
-                </section>
-                <section className="NavBar-Navigation">
-                    <button onClick={() => setDropdown(!dropdown)}>
-                        <FaAlignJustify/>
-                    </button>
-                    { dropdown ? 
-                        <ul className="NavBar-Options">
-                            <li><NavLink exact to="/">Home</NavLink></li>
-                            <li><NavLink to="/my-takes">My Takes</NavLink></li>
-                            <li><NavLink to="/profile">Profile</NavLink></li>
-                        </ul> 
-                        : null
-                    }
-                </section>
-            </nav>            
+        <div className="NavBar">
+            <section className="Banner-Title">
+                <h3>Just My Take</h3>
+            </section>
+            <section className="menu-container">
+                <button onClick={handleClick} className="dropdown-toggle">
+                    <FaAlignJustify/>
+                </button>
+                <nav ref={dropdownRef} className={`menu ${dropdown ? 'active' : 'inactive'}`}>
+                    <ul>
+                        <li><NavLink exact to="/">Home</NavLink></li>
+                        <li><NavLink to="/my-takes">My Takes</NavLink></li>
+                        <li><NavLink to="/profile">Profile</NavLink></li>
+                    </ul> 
+                </nav>            
+            </section>
         </div>
     );
 }
