65a948fe35b4adffc330d1c6015ba2d0a2df76b3
Adrian <reneavila1993@gmail.com>
2020-12-15 16:35:32 -0700

    Add validators and classes for input fields

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -6,11 +6,15 @@ const Login = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     
+    // Handles change values in input fields for login form
+
     const handleChange = ({ target }) => {
         return target.name === "username"
         ? setUsername(target.value)
         : setPassword(target.value);
     }
+    
+    // Checks for Input fields and if not empty passes them into login function
     
     const handleSubmit = (e) => {
         checkForInput();
@@ -23,10 +27,47 @@ const Login = () => {
         loginUser(user);
     }
 
-    const checkForInput = () => {
+    // Makes sure input fields are filled in and if not toggles validators.
 
+    const checkForInput = () => {
+        if(username === ""){
+            let usernameValidator = document.querySelector(".username-validator");
+            usernameValidator.classList.remove('hidden');
+            setTimeout(() => {
+                usernameValidator.classList.add('hidden');
+            }, 2000);
+        } else if(password === ""){
+            let passwordValidator = document.querySelector(".password-validator");
+            passwordValidator.classList.remove('hidden');
+            setTimeout(() => {
+                passwordValidator.classList.add('hidden');
+            }, 2000);
+        }
     }   
     
+    // Makes the POST request to our login route and if successful stores a JWT in local storage.
+    
+    const loginUser = (user) => {
+        fetch("http://localhost:8000/users/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+          },
+          body: JSON.stringify(user),
+        })
+        .then((response) => response.json())
+        .then((response) => {
+            if (password !== "" && response.message) {
+                handleLoginSuccess(false)
+            } else if( response.accessToken) {
+                handleLoginSuccess(true)
+                localStorage.setItem("token", response.accessToken);
+            }
+        });
+    }
+    
+    // Toggles notification whether or not the login attempt is successful.
+
     const handleLoginSuccess = (attempt) => {
         if(attempt === true){      
             let success = document.querySelector('.success');
@@ -41,25 +82,6 @@ const Login = () => {
                 failed.classList.add('hidden');
             }, 2000)
         }
-    }
-
-    const loginUser = (user) => {
-        fetch("http://localhost:8000/users/login", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(user),
-        })
-          .then((response) => response.json())
-          .then((response) => {
-            if (response.message) {
-                handleLoginSuccess(false)
-            } else {
-                handleLoginSuccess(true)
-                localStorage.setItem("token", response.accessToken);
-            }
-      });
     }
 
     return (
@@ -77,7 +99,7 @@ const Login = () => {
                         value={username} 
                         onChange={handleChange}
                         /><br/>
-                        <p className="validator hidden">Username Required</p>
+                        <p className="username-validator hidden">Username Required</p>
                     </div>
                     <div>
                         <label htmlFor="password">Password:</label><br/>
@@ -86,7 +108,7 @@ const Login = () => {
                         type="password" 
                         value={password} 
                         onChange={handleChange}/><br/>
-                        <p className="validator hidden">Password Required</p>
+                        <p className="password-validator hidden">Password Required</p>
                     </div>
                     <button>Login</button>
                     <NavLink to="/SignUp" className="signup-link">Create an Account</NavLink>
