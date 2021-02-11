274dc61eb8f0cd5ea58a849f9e6eaaefed5b3703
Adrian <reneavila1993@gmail.com>
2021-02-11 12:31:16 -0700

    Added styling globally to eliminate blue background on mobile click. Moved assets folder to public to get images working. Started basic framing of profile page

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,9 +4,15 @@ import './Profile.css'
 const Profile = () => {
     return (
         <div class="profile-page">
-            <p>My Profile</p>
-            <img src="../../assets/Blank-Avatar.jpg" alt="Blank Avatar"/>
-            
+            <p class="username">My Profile</p>
+            <img src="/assets/blankavatar.jpg" alt="Profile Picture"/>
+            <section class="about-me">
+                <p class="about-header">About:</p>
+                <p class="about-placeholder">Section is currently empty.</p>
+                <p class="about-header">Interests:</p>
+                <p class="about-placeholder">Section is currently empty.</p>
+            </section>
+            <button>Edit Profile</button>
         </div>
     )
 }
